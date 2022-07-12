774ff1c014612654f5d7257f9a0e4e64f7b787cb
Shon-Sang <dhsrbtka@naver.com>
2022-07-12 22:27:12 +0900

    NO 1166 but yes

--- backjoon_project/src/solving/silver3/Q0016_N1166.java
+++ backjoon_project/src/solving/silver3/Q0016_N1166.java
@@ -1,3 +1,5 @@
+package solving.silver3;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,16 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException{
+public class Q0016_N1166 {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk = new StringTokenizer(br.readLine());
-		List<Long> aList = new ArrayList<Long>();
+		List<Integer> aList = new ArrayList<Integer>();
 		int N = Integer.parseInt(stk.nextToken());
-		long L = Integer.parseInt(stk.nextToken());
-		long W = Integer.parseInt(stk.nextToken());
-		long H = Integer.parseInt(stk.nextToken());
+		int L = Integer.parseInt(stk.nextToken());
+		int W = Integer.parseInt(stk.nextToken());
+		int H = Integer.parseInt(stk.nextToken());
 		aList.add(L);
 		aList.add(W);
 		aList.add(H);
@@ -23,7 +25,7 @@ public class Main {
 		double end = minInt(aList); // 가장 변의 길이가 짧은애를 최대값으로 정해놓고 찾아야함
 		double target = 0;
 		double k;
-		for(int i =0; i<10000; i++) {
+		for(int i =0; i<8000; i++) {
 			target = (start+end)/2;
 			k = (long)(L/target) * (long)(W/target) * (long)(H/target);
 			if(N <= k) {
@@ -35,13 +37,24 @@ public class Main {
 		System.out.println(target);
 	}
 	
-	public static long minInt(List<Long> list) {
-		long result = 10000000;
-		for(long k : list) {
+	public static int maxInt(List<Integer> list) {
+		int result = 0;
+		for(int k : list) {
+			if(k>result) {
+				result = k;
+			}
+		}
+		return result;
+	}
+	
+	public static int minInt(List<Integer> list) {
+		int result = 10000000;
+		for(int k : list) {
 			if(k<result) {
 				result = k;
 			}
 		}
 		return result;
 	}
+
 }
