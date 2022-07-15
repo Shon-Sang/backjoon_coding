5eaa2043c613e2a01479fd75cf8dcbacc44cad73
Shon-Sang <dhsrbtka@naver.com>
2022-07-15 23:57:59 +0900

    YN 1347

---
+++ backjoon_project/src/solving/silver3/Q0019_N1347.java
@@ -0,0 +1,19 @@
+package solving.silver3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0019_N1347 {
+
+	public static void main(String[] args) {
+		List<Integer[]> posList = new ArrayList<Integer[]>();
+		Integer[] pos = {0, 0};
+		char direct = 'S';
+		posList.add(pos);
+		
+		int cnt = 5;
+		String inputDirect = "RRFRF";
+		
+	}
+
+}
